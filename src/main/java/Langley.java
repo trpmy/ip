85537b9756b9be50ab7e60630b24d9c7dcc90254
trpmy <trpmy@comp.nus.edu.sg>
2024-08-26 13:38:40 +0800

    Added echo functionality

--- src/main/java/Langley.java
+++ src/main/java/Langley.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 public class Langley {
     public static void main(String[] args) {
         String greeting = "Hello, this is Langley.";
-        String goodbye ="We will meet again.";
+        String goodbye = "We will meet again.";
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println(greeting);
-        System.out.println(goodbye);
+
+        while (true) {
+            System.out.print("You: ");
+            String userInput = scanner.nextLine();
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println(goodbye);
+                break;
+            } else {
+                System.out.println("Langley: " + userInput);
+            }
+        }
+
+        scanner.close();
     }
 }
