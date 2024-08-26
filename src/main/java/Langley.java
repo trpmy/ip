9ca7f08723a1d68539a42d91ac0fcd8a44b2df72
trpmy <mytran971@gmail.com>
2024-08-26 13:20:19 +0800

    Named bot Changed greeting Added goodbye

---
+++ src/main/java/Langley.java
@@ -0,0 +1,8 @@
+public class Langley {
+    public static void main(String[] args) {
+        String greeting = "Hello, this is Langley.";
+        String goodbye ="We will meet again.";
+        System.out.println(greeting);
+        System.out.println(goodbye);
+    }
+}
