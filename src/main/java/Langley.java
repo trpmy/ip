43e7ca87e975fe52fb40ad0d9ec86f0742bde4eb
trpmy <trpmy@comp.nus.edu.sg>
2024-08-26 17:02:52 +0800

    Added list history

--- src/main/java/Langley.java
+++ src/main/java/Langley.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Langley {
@@ -5,16 +6,24 @@ public class Langley {
         String greeting = "Hello, this is Langley.";
         String goodbye = "We will meet again.";
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> messages = new ArrayList<>();
 
         System.out.println(greeting);
 
         while (true) {
             System.out.print("You: ");
             String userInput = scanner.nextLine();
+
             if (userInput.equalsIgnoreCase("bye")) {
                 System.out.println(goodbye);
                 break;
+            } else if (userInput.equalsIgnoreCase("list")) {
+                System.out.println("Messages:");
+                for (String message : messages) {
+                    System.out.println(message);
+                }
             } else {
+                messages.add(userInput);
                 System.out.println("Langley: " + userInput);
             }
         }
